f0ac3210e95edd8332158d34a286ac7c436ea48f
JP Padilla <junnelpadilla307@gmail.com>
2023-10-08 15:59:27 -0700

    added background image

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <p>Header</p>
+      <hr></hr>
+      <p>Sub Header</p>
+      <hr></hr>
+      <p>Main Body</p>
+      <hr></hr>
+      <p>Footer</p>
     </div>
   );
 }
